889c11b5d01f313d900b12f513cfc7942d027070
psrivastav <preetisrivastav09@gmail.com>
2019-08-29 08:41:39 -0700

    Formatting as per standards

--- InteractiveEnglishDictionary.py
+++ InteractiveEnglishDictionary.py
@@ -29,29 +29,34 @@ from difflib import get_close_matches
 Data = json.load(open("C:\data.json"))
 
 
-def find_meaning(W):
-    if W in Data.keys():
-        return Data[W]
-    elif W.title() in Data.keys(): #Check with first letter captial like pronouns
-        return Data[W.title()]
-    elif W.upper() in Data.keys(): #Check acronyms
-        return (Data[W.upper()])
-    elif len(get_close_matches(W, Data.keys())) > 0:
-            Match = get_close_matches(W,Data.keys())[0]
-            UserInput = input("Do you mean %s instead of %s ?" %(Match, W))
-            if UserInput.lower() == "y":
-                W = Match
-                return Data[W]
-            else:
-                return "Please double check the spelling"
+def find_meaning(w):
+    if w in Data.keys():
+        return Data[w]
+    elif w.title() in Data.keys():  # Check with first letter Capital like pronouns
+        return Data[w.title()]
+    elif w.upper() in Data.keys():  # Check acronyms
+        return Data[w.upper()]
+    elif len(get_close_matches(w, Data.keys())) > 0:
+        match = get_close_matches(w,Data.keys())[0]
+        user_input = input("Do you mean %s instead of %s ?" %(match, w))
+        if user_input.lower() == "y":
+            w = match
+            return Data[w]
+        else:
+            return "Please double check the spelling"
     else:
         return "Word does not exist"
 
-Word = input("Enter the word")
-Output = find_meaning(Word.lower())
-if type(Output) == list:
-    for item in Output:
-        print("%s" %item)
-else:
-    print(Output)
-exit()
+
+def main():
+    word = input("Enter the word \n")
+    output = find_meaning(word.lower())
+    if type(output) == list:
+        for item in output:
+            print("%s" % item)
+    else:
+        print(output)
+
+
+if __name__ == '__main__':
+    main()
